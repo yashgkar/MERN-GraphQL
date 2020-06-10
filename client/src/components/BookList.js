3ccce8f86ef4d30b8a47ab0b4099f67d87d9a8e0
Yash Garudkar <yashgarudkar@gmail.com>
2020-06-10 15:44:39 +0530

    added animations, loading div

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
-import './BookList.css';
+import "./BookList.css";
 
 const getBooksQuery = gql`
-    {
-        books{
-            name
-            id
-        }
+  {
+    books {
+      name
+      id
     }
+  }
 `;
 
 class BookList extends Component {
-    displayBooks() {
-        var data = this.props.data;
-        if (data.loading) {
-            return (<div>Loading books...</div>);
-        } else {
-            return data.books.map(book => {
-                return (
-                    <li key={book.id}>{book.name}</li>
-                );
-            })
-        }
+    state = {
+        loading: false
     }
-    render() {
-        console.log(this.props);
-        return (
-            <div className="content">
-                <div className="book-list">
-                    <ul>
-                        {this.displayBooks()}
-                    </ul>
-                </div>
-
-            </div>
-        );
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div className="loader"></div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
     }
-
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="main">
+        <div className="content">
+          <div className="book-list">
+            <ul>{this.displayBooks()}</ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default graphql(getBooksQuery)(BookList);
